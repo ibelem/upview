fe15d176bf77203c640661993261053c825c21ea
Belem Zhang <belem.zhang@intel.com>
2021-09-18 10:09:26 +0800

    u

--- src/ui.js
+++ src/ui.js
@@ -1,7 +1,7 @@
 const show = () => {
     const sgn = document.querySelector('#sgn');
     const sgd = document.querySelector('#sgd');
-    const myRequest = new Request('./src/upview.json');
+    const myRequest = new Request('./src/upview_v2.json');
     const myRequest2 = new Request('./src/upview2.json');
     let signumber = ''
     let totalsigned = 0
@@ -25,9 +25,25 @@ const show = () => {
             let t = $("#"+i).html();
             $("#"+i).html(t);
         }
-        sgd.innerHTML = "<h3>已网签</h3>" + u
-        totalsigned = g.length
-        $("#sed").html(g.length)
+
+        let t2 = data.registered.toString()
+        let g2 = t2.split(",");
+        g2 = g2.sort();
+        let u2 = "";
+        for(let i of g2) {
+            console.log(i)
+            let q = "<span>" + i + "</span>"
+            u2 += q
+
+            i = i.replace('(复式)', '')
+            $("#"+i).addClass("registered");
+            let t = $("#"+i).html();
+            $("#"+i).html(t);
+        }
+
+        sgd.innerHTML = "<br><h3 class='registered'>已在交易中心预告登记</h3><div>随申办市民云'我的不动产'可查询。预告登记的前提是开发商已把房子的抵押从银行解押，这时房子是干净的，归到你名下，才能办购房贷款，银行再把你的当押金，给你放款。</div>" + u2 + "<h3 class='signed'>已网签 / 已签完预售合同</h3><div>只跟开发商签合同没做预告登记的会存在风险，虽然合同签了，在法律上这个房子还是归开发商所有，房子还是被开发商抵押给银行了。</div>" + u;
+        totalsigned = g.length + g2.length
+        $("#sed").html(g.length + g2.length)
     })
     .catch(console.error);
 
